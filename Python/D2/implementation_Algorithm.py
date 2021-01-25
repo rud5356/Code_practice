35d8b0e2417c97a91cc7abcb247a22695cd0016b
rud5356 <59816321+rud5356@users.noreply.github.com>
2021-01-25 10:50:14 +0900

    Update implementation_Algorithm.py
    Python-언어
    49,492 kb-메모리
    125 ms-실행시간
    222-코드길이

--- Python/D2/implementation_Algorithm.py
+++ Python/D2/implementation_Algorithm.py
@@ -1,17 +1,9 @@
 import sys
-sys.stdin = open("impl_exam_sample_input.txt", "r")
+sys.stdin = open("D:/Code_practice/Python/D2/impl_exam_sample_input.txt", "r")
 
 T = int(input())
 for test_case in range(1, T + 1):
-    n=str(input())
-    n1, n2 = 0, 0
-    for i in range(0,len(n)//2):
-        n1 += int(n[i])
-        n2 += int(n[len(n)-i-1])
-    if n1 == n2:
-        a = "LUCKY"
-    else:
-        a = "READY"
-    print(f"#{test_case} {a}")
-
-
+    n=str(input().strip())
+    n1 = sum(map(int,n[:len(n)//2]))
+    n2 = sum(map(int,n[len(n)//2:]))
+    print(f"#{test_case} LUCKY"  if n1==n2 else f"#{test_case} READY")
