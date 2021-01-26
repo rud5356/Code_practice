e6a63e7dd01cd0276eea3cc74c659a31c460b37c
rud5356 <59816321+rud5356@users.noreply.github.com>
2021-01-26 11:53:12 +0900

    Update date_calculator.py
    Python-언어
    53,728 kb-메모리
    124 ms-실행시간
    236-코드길이

--- Python/D2/date_calculator.py
+++ Python/D2/date_calculator.py
@@ -2,11 +2,8 @@ import sys
 sys.stdin = open("D:/Code_practice/Python/D2/date_calculator.txt", "r")
 
 T = int(input())
-for test_case in range(1, T + 1):
-    m1, d1, m2, d2 = map(int, input().split())
-    day = [31,28,31,30,31,30,31,31,30,31,30,31]
-    result = 0
-    for i in range(m1,m2):
-        result+=day[i-1]
-    result = result-d1+d2+1
+for test_case in range(1,T+1):
+    m1,d1,m2,d2=map(int,input().split()) #map 함수를 통해 각각의 변수에 대입
+    day=[31,28,31,30,31,30,31,31,30,31,30,31] # 월별 날짜 수 배열로 입력
+    result=sum(list([day[i-1] for i in range(m1,m2)]))+d2-d1+1 #해당 월의 날짜 수를 더하고 첫 달의 지난 날을 빼고 마지막 달의 지난 날을 더한 후 당일 1일 더하기
     print(f"#{test_case} {result}")
