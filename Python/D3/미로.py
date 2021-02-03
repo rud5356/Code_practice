ed5d6dacc23f599a7b2a88f4ebec34a72e426bc7
rud5356 <59816321+rud5356@users.noreply.github.com>
2021-02-03 10:41:18 +0900

    Update 미로.py

--- Python/D3/미로.py
+++ Python/D3/미로.py
@@ -1,28 +1,48 @@
 import sys
 sys.stdin = open("D:/Code_practice/Python/D3/input_미로.txt", "r")
 
-dx=[0,1,0,-1]
-dy=[1,0,-1,0]
+deg = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-#dfs함수 구현
-def DFS(x,y,s):
-    global val
-    #s+=graph[x][y] # 행렬 값을 더함
-    if val == 3 : return 1  #전체 더한 값이 최소합보다 크면 종료
-    # 행렬 끝까지 갔다면 합을 최소합으로 지정 후 종료
-    if y==(size-1) and x==(size-1) :
-        min = s
-        return
-    # dx의 크기만큼 돌면서 오른쪽으로 이동하다가 막히면 아래쪽으로 이동.
-    for i in range(len(dx)):
-        nextx = x+dx[i]
-        nexty = y+dy[i]
-        if graph[nextx][nexty] == 0:
-            DFS(nextx, nexty, s)
+def is_wall(r, c):
+  if r<0 or r>= 100 or c<0 or c>= 100:
+      return True
+  return False
 
-for i in range(1,int(input())+1):
-    size = int(input())
-    graph = [list(map(int, input().split())) for _ in range(100)]
-    val = 0
-    DFS(0,0,0)
-    print(f"#{i} {min}")
+def bfs(start, end):
+  Q.append(start)
+  cnt = 0
+  er, ec = end
+  while Q:
+      now = Q.pop(0)
+      nr, nc = now
+      maze[nr][nc] = 9
+
+      for dr, dc in deg:
+          tr, tc = nr + dr, nc + dc
+          if tr == er and tc == ec:
+              return 1
+          if not is_wall(tr, tc) and not maze[tr][tc]:
+              Q.append([tr, tc])
+  return 0
+
+
+
+for tc in range(10):
+  case_num = int(input())
+  maze = []
+  start, end = [], []
+  for i in range(100):
+      tmp = []
+      cnt = 0
+      for j in input():
+          tmp.append(int(j))
+          if int(j) == 2:
+              start = [i, cnt]
+          if int(j) == 3:
+              end = [i, cnt]
+          cnt += 1
+      maze.append(tmp)
+
+  Q = []
+
+  print(f"#{tc + 1} {bfs(start, end)}")
