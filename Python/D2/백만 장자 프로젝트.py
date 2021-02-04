5df2fb9ad3d1585b80525e01e92e332f994d457a
rud5356 <59816321+rud5356@users.noreply.github.com>
2021-02-04 18:01:36 +0900

    Create 백만 장자 프로젝트.py

---
+++ Python/D2/백만 장자 프로젝트.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open("D:/Code_practice/Python/D2/input_백만장자프로젝트.txt", "r")
+
+T = int(input())
+for test_case in range(1, T + 1):
+    n = int(input())
+    case = list(map(int,input().split()))
+    money = 0
+    result = 0
+    a=[]
+    while n!=0 :
+        for i in range(n-1):
+
+            money += case[i]
+        total = case[n-1]*(n-1)
+        print(money,total)
+        if total-money<0:
+            a.append(0)
+        else:
+            a.append(total-money)
+        money=0
+        n-=1
+    #if case[n-i-1]*(n-1)-money>0:
+    #    result = case[n-i-1]-money
+    print(a)
+    #print(result)
