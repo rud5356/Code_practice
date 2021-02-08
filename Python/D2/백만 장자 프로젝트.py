89a159d3a985a451d56b101ded8fb9ebf20185eb
rud5356 <59816321+rud5356@users.noreply.github.com>
2021-02-08 15:20:32 +0900

    Update 백만 장자 프로젝트.py
    Python-언어
    234,772 kb-메모리
    1,542 ms-실행시간
    419-코드길이

--- Python/D2/백만 장자 프로젝트.py
+++ Python/D2/백만 장자 프로젝트.py
@@ -1,28 +1,20 @@
 import sys
 sys.stdin = open("D:/Code_practice/Python/D2/input_백만장자프로젝트.txt", "r")
 
-T = int(input())
-for test_case in range(1, T + 1):
+for t in range(1, int(input()) + 1):
     n = int(input())
     case = list(map(int,input().split()))
-    money = 0
-    result = case[-1]*(n-1)
-    a=[]
-    while n<len(case):
-        for i in case[::-1]:
-            if i>result:
-                continue
-            money+=i
-        total = case[n-1]*(n-1)
-        print(money,total)
-        if total-money<0:
-            a.append(0)
-        else:
-            a.append(total-money)
-        money=0
-        n-=1
-    #if case[n-i-1]*(n-1)-money>0:
-    #    result = case[n-i-1]-money
-    print(a)
-    #print(result)
+    x,money,a = 0,0,0
+    m = case[-1]
+    for i in reversed(range(n-1)):
+        x += 1
+        money += case[i]
+        if case[i]>m:
+            total = m * (x-1) - (money-case[i])
+            a+=(total if total>0 else 0)
+            x,money=0,0
+            m = case[i]
+    a+=(m*x-money)
+    print(f"#{t} {a}")
+
 
